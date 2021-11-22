e9f2d882196eb06bcfecc59479f1faf337bc7274
yprawirocode <yusufprawiro14@gmail.com>
2021-11-23 03:34:05 +0700

    commit init

--- navigation/TravelNavigator.js
+++ navigation/TravelNavigator.js
@@ -2,24 +2,49 @@ import React from "react";
 
 import { StyleSheet } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from "@react-navigation/native";
-import { Image } from 'react-native'
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import HomeScreen from "../screens/HomeScreen";
 import FlightListScreen from "../screens/FlightListScreen";
 import DetailFlightScreen from "../screens/DetailFlightScreen";
 import HotelScreen from "../screens/HotelScreen";
 import TopNavigation from "./TopNavigation";
 
-const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 
-const TravelNavigator = (props) => { 
+const TravelNavigator = (props) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
+      <BottomTab.Navigator
+        screenOptions={{
+          tabBarStyle: [
+            {
+              position: 'absolute',
+              bottom: 30,
+              left: 20,
+              right: 20,
+              elevation: 3,
+              backgroundColor: '#ffffff',
+              borderRadius: 20,
+              height: 60,
+            }
+          ],
+          tabBarLabelStyle: {
+            fontSize: 12,
+          },
+        }}
+      >
+        <BottomTab.Screen
+          name="Home-"
           component={HomeScreen}
           options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={40} />
+            ),
             headerTransparent: true,
             headerShadowVisible: false,
             // shadowColor: 'transparent',
@@ -28,21 +53,69 @@ const TravelNavigator = (props) => {
               // backgroundColor: 'transparent',
               // shadowColor: 'transparent',
               // opacity: '50%',
-            },
-            // headerTitle: (props) => <TopNavigation {...props} />
+            }
           }}
         />
-        <Stack.Screen name="Hotel" component={HotelScreen} />
-        <Stack.Screen name="Flight" component={FlightListScreen} />
-        <Stack.Screen name="Gym" component={FlightListScreen} />
-        <Stack.Screen name="Laundry" component={FlightListScreen} />
-        <Stack.Screen name="Restaurant" component={FlightListScreen} />
-        <Stack.Screen name="Taxi" component={FlightListScreen} />
-        <Stack.Screen name="Train" component={FlightListScreen} />
-        <Stack.Screen name="Cafe" component={FlightListScreen} />
-        <Stack.Screen name="DetailFlight" component={DetailFlightScreen} />
-      </Stack.Navigator>
+        <BottomTab.Screen
+          name="Notifications"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Updates',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="bell" color={color} size={34} />
+            ),
+            tabBarBadge: 3,
+          }}
+        />
+        <BottomTab.Screen
+          name="Profile"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account" color={color} size={43} />
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
     </NavigationContainer>
+  )
+}
+
+const RootStack = createNativeStackNavigator();
+
+const BottomTabNav = (props) => {
+  return (
+    // <NavigationContainer>
+    <RootStack.Navigator initialRouteName="Home">
+      {/* <BottomTab.Screen name="BottomTab" component={BottomTabNav} /> */}
+      <RootStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTransparent: true,
+          headerShadowVisible: false,
+          // shadowColor: 'transparent',
+          title: '',
+          headerStyle: {
+            // backgroundColor: 'transparent',
+            // shadowColor: 'transparent',
+            // opacity: '50%',
+          },
+          // headerTitle: (props) => <TopNavigation {...props} />
+        }}
+      />
+      <RootStack.Screen name="Hotel" component={HotelScreen} />
+      <RootStack.Screen name="Flight" component={FlightListScreen} />
+      <RootStack.Screen name="Gym" component={FlightListScreen} />
+      <RootStack.Screen name="Laundry" component={FlightListScreen} />
+      <RootStack.Screen name="Restaurant" component={FlightListScreen} />
+      <RootStack.Screen name="Taxi" component={FlightListScreen} />
+      <RootStack.Screen name="Train" component={FlightListScreen} />
+      <RootStack.Screen name="Cafe" component={FlightListScreen} />
+      <RootStack.Screen name="DetailFlight" component={DetailFlightScreen} />
+    </RootStack.Navigator>
+    // </NavigationContainer>
   );
 };
 
